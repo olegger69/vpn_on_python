3cb68e1fecfe54062517386bf1a7e963c845d497
Oleg Gerasimchuk <olegavsim@gmail.com>
2024-12-30 07:48:42 +0300

    Написал функцию запуска vpn

--- vpn.py
+++ vpn.py
@@ -1,8 +1,10 @@
 import os
+from http.client import responses
+
 import requests
 
 
-def enable_vpn(proxy_host="", proxy_port=""):
+def enable_vpn(proxy_host="164.92.82.207", proxy_port="28131"):
     os.environ["HTTP_PROXY"] = f"socks5h://{proxy_host}:{proxy_port}"
     os.environ["HTTPS_PROXY"] = f"socks5h://{proxy_host}:{proxy_port}"
 
@@ -10,3 +12,15 @@ def enable_vpn(proxy_host="", proxy_port=""):
 def disable_vpn():
    os.environ.pop("HTTP_PROXY", None)
    os.environ.pop("HTTPS_PROXY", None)
+
+
+def send_requests():
+    response = requests.get("https://httpbin.org/ip", timeout=20)
+    print(response.json())
+
+
+send_requests()
+enable_vpn()
+send_requests()
+disable_vpn()
+send_requests()
