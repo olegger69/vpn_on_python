899fa92644ef8d4c2051291f8b2a4d2addbc7d8d
Oleg Gerasimchuk <olegavsim@gmail.com>
2024-12-30 06:43:02 +0300

    Написал функцию включения vpn

--- vpn.py
+++ vpn.py
@@ -1,2 +1,6 @@
 import os
 import requests
+
+def enable_vpn(proxy_host="", proxy_port=""):
+    os.environ["HTTP_PROXY"] = f"socks5h://{proxy_host}:{proxy_port}"
+    os.environ["HTTPS_PROXY"] = f"socks5h://{proxy_host}:{proxy_port}"
