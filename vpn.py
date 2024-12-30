ed97ca1acf1c469108152013e5f2bae2c688328f
Oleg Gerasimchuk <olegavsim@gmail.com>
2024-12-30 06:50:03 +0300

    Написал функцию выключения vpn

--- vpn.py
+++ vpn.py
@@ -1,6 +1,12 @@
 import os
 import requests
 
+
 def enable_vpn(proxy_host="", proxy_port=""):
     os.environ["HTTP_PROXY"] = f"socks5h://{proxy_host}:{proxy_port}"
     os.environ["HTTPS_PROXY"] = f"socks5h://{proxy_host}:{proxy_port}"
+
+
+def disable_vpn():
+   os.environ.pop("HTTP_PROXY", None)
+   os.environ.pop("HTTPS_PROXY", None)
